7e6b4e4937cbaf11fced23afd7619ce54a8413b5
maadlog <martin.loguancio@gmail.com>
2015-12-21 10:11:03 -0300

    Json Working, all hail Angular

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@
     var app = angular.module('portfolio',[]);
 
     app.controller("portfolioCtrl",['$http',function($http) {
-        this.projects = proj;
+
+        var portfolio = this;
+
+        $http.get('http://maadlog.github.io/js/projects.json')
+            .then(function success(response) {
+                portfolio.projects = response.data.projects;
+
+        });
     }]);
-
-    var proj =[
-        {"title":"Project Hades", "image":"url", "description":"This is a hell of a project"},
-        {"title":"Project Hermes", "image":"url", "description":"Delivery Fast and Efficiently"},
-        {"title":"Project Zeus", "image":"url", "description":"Will fuck a duck just for fun"}
-    ];
-
 })();
