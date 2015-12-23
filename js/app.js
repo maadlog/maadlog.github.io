d1b8ee0b3a9ef1ef33b16a01ecdd985a167d3fcf
maadlog <martin.loguancio@gmail.com>
2015-12-23 13:20:06 -0300

    Removed Carousel, added nav-pills. Oh, and finished the page (? Neato. Waiting for more ideas

--- js/app.js
+++ js/app.js
@@ -23,6 +23,26 @@
             });
     }]);
 
+    app.controller("toolsCtrl",['$http',function($http) {
+
+        var myself = this;
+
+        $http.get('./res/tools.json')
+            .then(function success(response) {
+                myself.tools = response.data.tools;
+            });
+    }]);
+
+    app.controller("experienceCtrl",['$http',function($http) {
+
+        var myself = this;
+
+        $http.get('./res/experience.json')
+            .then(function success(response) {
+                myself.jobs = response.data.jobs;
+            });
+    }]);
+
     app.directive("projects",function(){
         return{
             restrict:'E',
@@ -30,11 +50,22 @@
             };
     });
 
-    app.directive("curriculum",function(){
+    app.directive("curriculumVitae",function(){
         return{
             restrict:'E',
-            templateUrl:'./html/curriculum.html'
-        };
+            templateUrl:'./html/curriculum.html'};
+    });
+
+    app.directive("tools",function(){
+        return{
+            restrict:'E',
+            templateUrl:'./html/tools.html'};
+        });
+
+    app.directive("experience",function(){
+        return{
+            restrict:'E',
+            templateUrl:'./html/experience.html'};
     });
 
 })();
--- js/jquery.js
+++ js/jquery.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-    $('#CV').on('click',function(event){
-        event.preventDefault();
-        event.stopPropagation();
-        $('curriculum').slideToggle('slow');
-    });
-
     var toggleAnimation = function(){
         $(this).toggleClass('animatedPic');
     };
 
-    $('#profile').on('mouseenter',toggleAnimation).on('mouseleave',toggleAnimation).on('touchend',toggleAnimation);
+    $('#structureLink').on('click', function (event){
+        event.preventDefault();
+        event.stopPropagation();
+    });
 
+    $('[data-toggle="popover"]').popover(); //Initialize PopOvers.
+
+    $('#profile').on('mouseenter',toggleAnimation).on('mouseleave',toggleAnimation).on('touchend',toggleAnimation);
 });
